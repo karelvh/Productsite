796f66da9245c8a75e0e0440cb9050c325899663
vhKarel <karel@kvHs-MBA-Pro.local>
2015-01-03 19:25:12 +0100

    Addad an xml reader and media

--- assets/js/readXML.js
+++ assets/js/readXML.js
@@ -1,83 +1,23 @@
+var specs = [];
+
 $(function(){
-    console.log("dom ready");
-    downloadCars();
+    //console.log("dom ready");
+    downloadSpecs();
 });
 
-function downloadCars(){
+function downloadSpecs(){
     $.ajax({
-//        indien datatype niet meegeven krijg je gwoon Text
         dataType: "xml",
         url: "contentEN.xml",
         type: "GET",
         success: function(xml) {
-            console.log(xml);
-            $(xml).find('car').each(function(){
-                var Titles = $(this).find('Title').text();
-                var Manufacturers = $(this).find('Manufacturer').text();
-                $("<li class='list-group-item'></li>").html(Titles + ", " + Manufacturers).appendTo("#carsul");
+//            console.log(xml);
+            $(xml).find('spec').each(function(){
+                var fa = $(this).find('Fa').text();
+                var title = $(this).find('Title').text();
+                var specText = $(this).find('Text').text();
+                $('<div class="col-md-3"><i class="fa '+fa+'"></i><h4>'+title+'</h4><p>'+specText+'</p>').appendTo('.specsdiv');
             });
         }
     });
-}
-
-
-
-
-
-
-
-
-
-
-
-var photos = [];
-
-$(function(){
-    console.log("dom ready");
-    downloadPhotos();
-});
-
-function downloadPhotos(){
-    //ajax getJSON
-    $.ajax({
-        dataType: "json",
-        url: "api.php",
-        data: "",
-        success: function(response){
-//            console.log(response);
-            photos = response;
-            showPhotos();
-        }
-    });
-}
-
-function showPhotos(){
-//    <li>
-//        <a href=""/>
-//    </li>
-//    ZELF GEPROBEERD
-//    $.each( "<li id='"+val+"'><a href='/img/reboot/large/"+key+"'></li>" ).appendTo( ".Stage" );
-    var l = photos.length;
-    if(l > 0){
-        var image;
-        for(var i = 0; i < l; i++){
-            image = photos[i];
-            addImage(image);
-        }
-    }
-}
-
-function addImage(img){
-    // id, fotourl, description
-//    <li>
-//        <img src=""/>
-//        <figcaption>description</figcaption>
-//    </li>
-    var bobTheHtmlBuilder = '<li class="img">';
-    bobTheHtmlBuilder += '<a href="img/reboot/large/'+img.fotourl+'"data-lightbox="image-1" data-title="'+img.description+'"><img src="img/reboot/small/'+img.fotourl+'" alt="'+img.description+'" title="'+img.description+'"/></a>';
-//    bobTheHtmlBuilder += '<img src="img/reboot/small/'+img.fotourl+'" alt="'+img.description+'" title="'+img.description+'"/>';
-    bobTheHtmlBuilder += '<figcaption><a href="img/reboot/large/'+img.fotourl+'"data-lightbox="image-1" data-title="'+img.description+'">'+img.description+'</a></figcaption>';
-    bobTheHtmlBuilder += '</li>';
-    
-    $('#photos').append(bobTheHtmlBuilder);
 }
